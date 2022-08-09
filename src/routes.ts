5fa5d6b5ffda59576882e6ab20479d12a833c866
PauloMtz <pramosmtz@gmail.com>
2022-08-09 13:25:03 -0300

    Tratando erros

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response, request } from "express";
 const router = Router();
 
 router.get('/api/teste', (req: Request, res: Response) => {
+    //throw new Error('Ocorreu um erro inesperado.');
     return res.json({app_name: 'e-Pizzaria'})
 })
 
